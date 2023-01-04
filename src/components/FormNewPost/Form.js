bfd19e6113cc1d42d3ba0b8367b44674952d488e
abdullahi <moh.abdullahi2003@gmail..com>
2023-01-04 11:00:42 +0100

    done

--- src/components/FormNewPost/Form.js
+++ src/components/FormNewPost/Form.js
@@ -1,23 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom"
+import { Editor } from 'react-draft-wysiwyg';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
 import Classes from './Form.module.css'
 
-const form = () => (
-    <div className={Classes.Form}>
-        <h4>New Post</h4>
-        <section>
-            <div>
-                <label for="tittle">Title</label>
-                <input type='text' id="tittle" placeholder="Tittle" title="title" />
-            </div>
-            <div>
-                <label for="article" >Article</label>
-                <textarea id="article" title="article" placeholder="Your Article goes here" />
-            </div>
-            <Link to="/">Publish</Link>
-        </section>
-    </div>
-)
+const form = () => {
+    // const wrapperClassName = `${Classes.editor} ${Classes.wrapper}`;
+    return (
+        <div className={Classes.Form}>
+            <h4>New Post</h4>
+            <section>
+                {/* <form> */}
+                <div>
+                    <label for="tittle">Title</label>
+                    <input type='text' id="tittle" placeholder="Tittle" title="title" required />
+                </div>
+                <div>
+                    <label for="article" >Article</label>
+                    {/* <textarea id="article" title="article" placeholder="Your Article goes here" required /> */}
+                    <Editor
+                        wrapperClassName={Classes.wrapper}
+                        editorClassName={Classes.editor}
+                        toolbarClassName={Classes.toolbar} />
+
+                </div>
+                {/* <button type="submit" href="/">Publish</button> */}
+                <div><Link to="/">Publish</Link></div>
+                {/* </form> */}
+            </section>
+        </div>
+    )
+}
 
 export default form
